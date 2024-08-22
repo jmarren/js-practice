912904ac7c7653c8650f0f80e950e41e828d6d36
jmarren <jmarren99@gmail.com>
2024-08-22 13:13:16 -0700

    Nested ArrowClass working properly

--- static/js/webgame.js
+++ static/js/webgame.js
@@ -3,27 +3,40 @@ window.onload = function() {
     constructor(gameTitle) {
       this.gameTitle = gameTitle
       this.gameTitleElement = document.getElementById("game-title")
-      this.arrow = {
-        domElement: document.getElementById("arrow"),
-        theta: 0,
-      }
       this.gameCenter = {
         x: (500 / 2) + 8,
         y: (500 / 2) + 26,
       }
+      this.arrowInstance = new this.ArrowClass(this.gameCenter)
     }
-    init() {
-      this.arrow.domElement.style.left = `${this.gameCenter.x - 10}px`
-      this.arrow.domElement.style.top = `${this.gameCenter.y - 60}px`
-
-      window.addEventListener("mousemove", (e) => {
+    ArrowClass = class {
+      constructor(gameCenter) {
+        this.domElement = document.getElementById("arrow");
+        this.theta = 0;
+        this.gameCenter = gameCenter
+      }
+      getTheta(e) {
         const opp = e.clientX - this.gameCenter.x
         const adj = e.clientY - this.gameCenter.y
         console.log("adjacent: ", adj)
         console.log("opposite: ", opp)
-        this.arrow.theta = Math.atan2(adj, opp) + (Math.PI / 180) * 90
-        console.log("theta: ", this.arrow.theta * 180 / Math.PI, "degrees")
+        this.theta = Math.atan2(adj, opp) + (Math.PI / 180) * 90
+        console.log("theta: ", this.theta * 180 / Math.PI, "degrees")
         this.render()
+      }
+      init() {
+        this.domElement.style.left = `${this.gameCenter.x - 10}px`
+        this.domElement.style.top = `${this.gameCenter.y - 60}px`
+      }
+      render() {
+        document.documentElement.style.setProperty("--arrow-rotation", `${this.theta}rad`)
+      }
+    }
+
+    init() {
+      this.arrowInstance.init()
+      window.addEventListener("mousemove", (e) => {
+        this.arrowInstance.getTheta(e)
       })
       window.addEventListener("pointerdown", () => {
 
@@ -31,11 +44,6 @@ window.onload = function() {
       window.addEventListener("pointerup", () => {
       })
     }
-
-    render() {
-      document.documentElement.style.setProperty("--arrow-rotation", `${this.arrow.theta}rad`)
-    }
-
   }
 
   const myPage = new Page("a old game")
