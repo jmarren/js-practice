9cb1f8e3554ff4c7ecc6ddf02681eadfb39e52ff
jmarren <jmarren99@gmail.com>
2024-08-22 12:56:02 -0700

    arrow spinning properly 2nd go-round

--- static/js/webgame.js
+++ static/js/webgame.js
@@ -3,69 +3,39 @@ window.onload = function() {
     constructor(gameTitle) {
       this.gameTitle = gameTitle
       this.gameTitleElement = document.getElementById("game-title")
-      this.moveableBlock = document.getElementById("moveable-block")
-      this.moveableBlockState = {
-        positionX: 0,
-        positionY: 0,
-      }
-      this.arrowContainerElement = document.getElementById("arrow-container")
-      this.arrowElement = document.getElementById("arrow")
       this.arrow = {
-        rotation: 0,
-        windup: 0,
-        windupTime: 0,
-        windupInterval: null,
-        isShooting: false,
+        domElement: document.getElementById("arrow"),
+        theta: 0,
       }
-
-      let arrowClientRect = this.arrowElement.getBoundingClientRect()
-      this.centerLocation = {
-        x: arrowClientRect.x + (arrowClientRect.width / 2),
-        y: arrowClientRect.y + (arrowClientRect.height / 2),
+      this.gameCenter = {
+        x: (500 / 2) + 8,
+        y: (500 / 2) + 26,
       }
     }
     init() {
+      this.arrow.domElement.style.left = `${this.gameCenter.x - 10}px`
+      this.arrow.domElement.style.top = `${this.gameCenter.y - 60}px`
+
       window.addEventListener("mousemove", (e) => {
-        let opposite = e.clientX - this.centerLocation.x
-        let adjacent = e.clientY - this.centerLocation.y
-        const theta = (Math.atan2(opposite, adjacent) * 180) / Math.PI
-        this.arrow.rotation = theta
-        this.renderArrowContainer()
+        const opp = e.clientX - this.gameCenter.x
+        const adj = e.clientY - this.gameCenter.y
+        console.log("adjacent: ", adj)
+        console.log("opposite: ", opp)
+        this.arrow.theta = Math.atan2(adj, opp) + (Math.PI / 180) * 90
+        console.log("theta: ", this.arrow.theta * 180 / Math.PI, "degrees")
+        this.render()
       })
       window.addEventListener("pointerdown", () => {
-        this.arrow.windupInterval = setInterval(() => {
-          console.log(this.arrow.windupTime)
-          this.arrow.windupTime = this.arrow.windupTime + 0.1
-          this.arrow.windup = ((60 * this.arrow.windupTime) - 9) / (this.arrow.windupTime + 1)
-          console.log(this.arrow.windup)
-          this.renderPointer()
-        }, 10
-        )
+
       })
       window.addEventListener("pointerup", () => {
-        clearInterval(this.arrow.windupInterval)
-        this.arrow.windupTime = 0
-        this.arrow.windup = 0
-        this.renderPointer()
       })
     }
 
-    renderPointer() {
-      if (!this.arrow.isShooting) {
-        this.arrowElement.style.transform = `translateY(${60 - this.arrow.windup}px)`
-      } else {
-        // this.arrowElement.style.transform = `translateY(${}`
-      }
+    render() {
+      document.documentElement.style.setProperty("--arrow-rotation", `${this.arrow.theta}rad`)
     }
 
-
-    renderKeyPress() {
-      const transform = `translateX(${this.moveableBlockState.positionX * 10}px) translateY(${this.moveableBlockState.positionY * 10}px)`
-      this.moveableBlock.style.transform = transform
-    }
-    renderArrowContainer() {
-      this.arrowContainerElement.style.transform = `rotate(${-1 * this.arrow.rotation}deg)`
-    }
   }
 
   const myPage = new Page("a old game")
