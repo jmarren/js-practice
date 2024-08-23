e497e58b5b9b1b2f6687a552474f2f230403d45a
jmarren <jmarren99@gmail.com>
2024-08-23 13:46:31 -0700

    started pool game

---
+++ static/js/pool.js
@@ -0,0 +1,205 @@
+
+const GAMEWIDTH = 500
+const GAMEHEIGHT = 500
+const MARGINLEFT = 8
+const BALLDIAMETER = 20
+const BALLRADIUS = BALLDIAMETER / 2
+const MARGINTOP = 26
+const CUEWIDTH = 10
+const CUETIPWIDTH = CUEWIDTH / 2
+const GAMECENTERX = (GAMEWIDTH / 2) + MARGINLEFT
+const GAMECENTERY = (GAMEWIDTH / 2) + MARGINTOP
+const TOPROWY = MARGINTOP + GAMEHEIGHT / 4
+const BALLPOSITIONS = [
+  { x: GAMECENTERX, y: GAMECENTERY },
+  { x: GAMECENTERX, y: TOPROWY },
+  { x: GAMECENTERX - BALLDIAMETER, y: TOPROWY },
+  { x: GAMECENTERX + BALLDIAMETER, y: TOPROWY },
+  { x: GAMECENTERX - (2 * BALLDIAMETER), y: TOPROWY },
+  { x: GAMECENTERX + (2 * BALLDIAMETER), y: TOPROWY },
+  { x: GAMECENTERX - (BALLDIAMETER / 2), y: TOPROWY + BALLDIAMETER * (0.88) },
+  { x: GAMECENTERX + (BALLDIAMETER / 2), y: TOPROWY + BALLDIAMETER * (0.88) },
+  { x: GAMECENTERX - (3 * BALLDIAMETER / 2), y: TOPROWY + BALLDIAMETER * (0.88) },
+  { x: GAMECENTERX + (3 * BALLDIAMETER / 2), y: TOPROWY + BALLDIAMETER * (0.88) },
+  { x: GAMECENTERX, y: TOPROWY + 2 * BALLDIAMETER * 0.88 },
+  { x: GAMECENTERX - BALLDIAMETER, y: TOPROWY + 2 * (BALLDIAMETER * 0.88) },
+  { x: GAMECENTERX + BALLDIAMETER, y: TOPROWY + 2 * (BALLDIAMETER * 0.88) },
+  { x: GAMECENTERX - (BALLDIAMETER / 2), y: TOPROWY + 3 * BALLDIAMETER * (0.88) },
+  { x: GAMECENTERX + (BALLDIAMETER / 2), y: TOPROWY + 3 * BALLDIAMETER * (0.88) },
+  { x: GAMECENTERX, y: TOPROWY + 4 * BALLDIAMETER * 0.88 },
+]
+
+
+window.onload = function() {
+  class Game {
+    constructor() {
+      this.cue = new Cue()
+      this.ball = new Ball()
+      this.gameCenter = {
+        x: GAMECENTERX,
+        y: GAMECENTERY,
+      }
+      this.ballInstances = []
+      this.cueInstance = new Cue()
+      this.state = 0
+      this.whiteball = this.ballInstances[0]
+    }
+
+    get stateString() {
+      switch (this.state) {
+        case 0:
+          return "lining up"
+        case 1:
+          return "whiteball placed"
+        case 2:
+          return "winding up"
+        default:
+          return "undefined"
+      }
+    }
+
+
+    Init() {
+      for (let i = 0; i < BALLPOSITIONS.length; i++) {
+        const ball = new Ball(i, BALLPOSITIONS[i].x, BALLPOSITIONS[i].y)
+        ball.Init()
+        this.ballInstances.push(ball)
+      }
+
+
+      window.addEventListener("mousemove", (e) => {
+        if (this.state === 0) {
+          this.ballInstances[0].Move(e)
+        }
+        if (this.state === 1) {
+          this.cueInstance.Rotate(e)
+        }
+      })
+      window.addEventListener("click", () => {
+        if (this.state === 0) {
+          this.state = 1
+          const whiteBallPosition = this.ballInstances[0].getPosition
+          this.cueInstance.PlaceBall(whiteBallPosition)
+        }
+      })
+      window.addEventListener("pointerdown", () => {
+        if (this.state === 1) {
+          this.cueInstance.windupInterval = setInterval(() => {
+            this.cueInstance.WindUp()
+          }, 100)
+          this.state = 2
+        }
+      })
+      window.addEventListener("pointerup", () => {
+        if (this.state === 2) {
+          console.log("up!")
+          this.cueInstance.ClearWindup()
+        }
+      })
+    }
+  }
+
+  class Cue {
+    constructor() {
+      this.outerDomElement = document.getElementById("cue-container")
+      this.innerDomElement = document.getElementById("cue")
+
+      this.position = {
+        x: 0,
+        y: 0,
+      }
+      this.outerTransform = {
+        x: 0,
+        y: 0,
+        theta: 0,
+      }
+      this.innerTransform = {
+        x: 0,
+        y: 0,
+      }
+      this.windup = {
+        elapsed: 0,
+        interval: null,
+      }
+    }
+    set windupInterval(interval) {
+      this.windup.interval = interval
+    }
+
+    ClearWindup() {
+      clearInterval(this.windup.interval)
+    }
+    PlaceBall(position) {
+      this.position = {
+        x: position.x,
+        y: position.y,
+      }
+      this.outerDomElement.style.transformOrigin = `${(BALLRADIUS / 2)}px ${-1 * BALLDIAMETER + 5}px`
+      this.outerTransform = {
+        x: (BALLRADIUS / 2) - (CUETIPWIDTH / 4),
+        y: BALLDIAMETER + 5,
+        theta: 0,
+      }
+
+      this.#render()
+    }
+    Rotate(e) {
+      const adjacent = e.clientY - this.position.y
+      const opposite = e.clientX - this.position.x
+      this.outerTransform.theta = -1 * Math.atan2(opposite, adjacent)
+      this.#render()
+    }
+
+    WindUp() {
+      console.log("windupInterval: ", this.windupInterval)
+      console.log("windup.interval: ", this.windup.interval)
+      console.log("windup.elapsed: ", this.windup.elapsed)
+      this.windup.elapsed += 0.1
+      this.innerTransform.y = (60 * this.windup.elapsed - 5.8) / (this.windup.elapsed + 1)
+      this.#render()
+    }
+
+    Move(e) {
+      this.position.x = e.clientX - BALLRADIUS
+      this.position.y = e.clientY - BALLRADIUS
+      this.#render()
+    }
+    #render() {
+      this.outerDomElement.style.left = `${this.position.x}px`
+      this.outerDomElement.style.top = `${this.position.y}px`
+      this.outerDomElement.style.transform = `translateX(${this.outerTransform.x}px) translateY(${this.outerTransform.y}px) rotate(${this.outerTransform.theta}rad) `
+      this.innerDomElement.style.transform = `translateY(${this.innerTransform.y}px)`
+    }
+
+  }
+
+  class Ball {
+    constructor(number, positionX, positionY) {
+      this.number = number
+      this.domElement = document.getElementById(`ball-${number}`)
+      this.position = {
+        x: positionX,
+        y: positionY,
+      }
+    }
+    get getPosition() {
+      return this.position
+    }
+    Move(e) {
+      this.position.x = e.clientX - BALLRADIUS
+      this.position.y = e.clientY - BALLRADIUS
+      this.#render()
+    }
+    Init() {
+      this.domElement.style.left = `${this.position.x}px`
+      this.domElement.style.top = `${this.position.y}px`
+    }
+    #render() {
+      this.domElement.style.left = `${this.position.x}px`
+      this.domElement.style.top = `${this.position.y}px`
+    }
+  }
+
+  const game = new Game();
+  game.Init()
+}
--- static/js/webgame.js
+++ static/js/webgame.js
@@ -8,46 +8,159 @@ window.onload = function() {
         y: (500 / 2) + 26,
       }
       this.arrowInstance = new this.ArrowClass(this.gameCenter)
+      this.greenBall = new this.BallClass("green", 1)
+      this.yellowBall = new this.BallClass("yellow", 2)
+      this.blueBall = new this.BallClass("blue", 3)
+      this.redBall = new this.BallClass("red", 4)
+      this.state = 0
     }
+
+    get stateString() {
+      switch (this.state) {
+        case 0:
+          return "aiming"
+        case 1:
+          return "winding-up";
+        case 2:
+          return "shot"
+        default:
+          return "undefined state";
+      }
+    }
+
+    BallClass = class {
+      constructor(color, id) {
+        this.position = {
+          x: id,
+          y: id,
+        }
+        this.color = color
+        this.domElement = document.getElementById(`ball-${id}`)
+        this.id = id
+      }
+      Init() {
+        this.domElement.style.left = this.position.x
+        this.domElement.style.top = this.position.y
+        this.domElement.style.backgroundColor = this.color
+      }
+
+    }
+
     ArrowClass = class {
       constructor(gameCenter) {
         this.domElement = document.getElementById("arrow");
         this.theta = 0;
+        this.position = {
+          x: gameCenter.x - 10,
+          y: gameCenter.y - 60,
+        }
+        this.windup = {
+          elapsed: 0,
+          speed: 0,
+          interval: null,
+        }
+        this.shoot = {
+          interval: null,
+          elapsed: 0,
+          speed: 0,
+        }
         this.gameCenter = gameCenter
       }
-      getTheta(e) {
+      Init() {
+        document.documentElement.style.setProperty("--arrow-x", `${this.position.x}`)
+        document.documentElement.style.setProperty("--arrow-y", `${this.position.y}`)
+      }
+      Reset() {
+        clearInterval(this.windup.interval)
+        this.position = {
+          x: this.gameCenter.x - 10,
+          y: this.gameCenter.y - 60,
+        }
+        this.windup = {
+          elapsed: 0,
+          speed: 0,
+          interval: null,
+        }
+        this.#render()
+      }
+      Spin(e) {
         const opp = e.clientX - this.gameCenter.x
         const adj = e.clientY - this.gameCenter.y
-        console.log("adjacent: ", adj)
-        console.log("opposite: ", opp)
         this.theta = Math.atan2(adj, opp) + (Math.PI / 180) * 90
-        console.log("theta: ", this.theta * 180 / Math.PI, "degrees")
-        this.render()
+        this.#render()
       }
-      init() {
-        this.domElement.style.left = `${this.gameCenter.x - 10}px`
-        this.domElement.style.top = `${this.gameCenter.y - 60}px`
+      WindUp() {
+        this.windup.elapsed = this.windup.elapsed + 0.01
+        this.windup.speed = 0.5 / (this.windup.elapsed * this.windup.elapsed + 1)
+        console.log(this.windup.speed)
+        const deltaX = this.windup.speed * Math.sin(this.theta)
+        const deltaY = this.windup.speed * Math.cos(this.theta)
+        this.position.x = this.position.x - deltaX
+        this.position.y = this.position.y + deltaY
+        this.#render()
       }
-      render() {
+      Shoot() {
+        this.shoot.elapsed = this.shoot.elapsed + 1
+        this.shoot.speed = (5 * Math.pow(this.shoot.elapsed, 3)) / (Math.pow(this.shoot.elapsed, 3) + 1)
+        if (this.position.x > 508) {
+          this.theta = this.theta - Math.PI / 2
+        }
+        if (this.position.x < 8) {
+          this.theta = this.theta + Math.PI / 2
+        }
+        if (this.position.y < 26) {
+          this.theta = Math.PI - this.theta
+        }
+        if (this.position.y > 526) {
+          this.theta = Math.PI + this.theta
+        }
+        const deltaX = this.shoot.speed * Math.sin(this.theta)
+        const deltaY = this.shoot.speed * Math.cos(this.theta)
+        this.position.x = this.position.x + deltaX
+        this.position.y = this.position.y - deltaY
+        console.log(this.position.x)
+
+        this.#render()
+      }
+      #render() {
+        document.documentElement.style.setProperty("--arrow-x", this.position.x)
+        document.documentElement.style.setProperty("--arrow-y", this.position.y)
         document.documentElement.style.setProperty("--arrow-rotation", `${this.theta}rad`)
       }
     }
 
     init() {
-      this.arrowInstance.init()
+      this.arrowInstance.Init()
+      this.greenBall.Init()
+      this.yellowBall.Init()
+      this.blueBall.Init()
+      this.redBall.Init()
       window.addEventListener("mousemove", (e) => {
-        this.arrowInstance.getTheta(e)
+        if (this.state == 0) {
+          this.arrowInstance.Spin(e)
+        }
       })
       window.addEventListener("pointerdown", () => {
-
+        if (this.state == 0) {
+          this.state = 1
+          this.arrowInstance.windup.interval = setInterval(() => {
+            this.arrowInstance.WindUp()
+          }, 10)
+        }
       })
       window.addEventListener("pointerup", () => {
+        // this.arrowInstance.Reset()
+        this.state = 2
+        this.arrowInstance.shoot.interval = setInterval(() => {
+          this.arrowInstance.Shoot()
+        }, 1)
       })
     }
   }
 
   const myPage = new Page("a old game")
   myPage.init()
+  console.log(myPage.stateString)
 }
 
 
